5698533c5eab53789886b8a42c12959895c5f425
lee-andrew <lee.andrew076@gmail.com>
2017-12-30 23:14:09 -0500

    Update task.sh
    Wrap private key with quotes

--- tasks/post-state/task.sh
+++ tasks/post-state/task.sh
@@ -7,7 +7,7 @@ pushd vpn-status-repo/status
   curl $discord_webhook -d "{\"content\":\"$message\"}"
 
   if [ $state == 'off' ]; then
-    echo $private_key > /tmp/id_rsa
+    echo "$private_key" > /tmp/id_rsa
     chmod 400 /tmp/id_rsa
     $(ssh -i /tmp/id_rsa -o StrictHostKeyChecking=no $ssh_user@$ssh_host "$ssh_command")
   fi
