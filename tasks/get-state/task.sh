3443a0b905e82ba20e93743175863c66b4fe35c1
lee-andrew <lee.andrew076@gmail.com>
2017-12-30 22:45:58 -0500

    Added tasks and status files for vpn status

---
+++ tasks/get-state/task.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+echo $private_key > /tmp/id_rsa
+chmod 400 /tmp/id_rsa
+
+result=$(ssh -i /tmp/id_rsa -o StrictHostKeyChecking=no $ssh_user@$ssh_host "curl ifconfig.co/country" | grep "United States")
+
+pushd vpn-state-repo/status
+
+  if [ $result == "United States" ]; then
+    if [ $(cat state) == 'off' ]; then
+      echo 'on' > state
+    fi
+  else
+    if [ $(cat state) == 'on' ]; then
+      echo 'off' > state
+    fi
+  fi
+
+  if [[ -n $(git status --porcelain) ]]; then
+    git config --global user.name $git_user
+    git config --global user.email $git_email
+    git add .
+    git commit -m "state change"
+  fi
+
+popd
+
+shopt -s dotglob
+cp -r vpn-state-repo/* output
+
---
+++ tasks/post-state/task.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pushd vpn-status-repo/status
+
+  state=$(cat state)
+  message="VPN is $state"
+  curl $discord_webhook -d "{\"content\":\"$message\"}"
+
+  if [ $state == 'off' ]; then
+    echo $private_key > /tmp/id_rsa
+    chmod 400 /tmp/id_rsa
+    $(ssh -i /tmp/id_rsa -o StrictHostKeyChecking=no $ssh_user@$ssh_host "$ssh_command")
+  fi
+
+popd
